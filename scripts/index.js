c15e07465587ce18afe21509b3e39e9b5a5b6c49
largentba21 <largentba21@gmail.com>
2021-10-17 13:28:38 -0500

    Adding context menu to quickly toggle module state

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,47 @@ Hooks.once("init", () => {
   // Adding Critical Threshold field in weapon details menu
   injectCriticalThresholdWeaponField();
 
+  // Adding context menu to quickly toggle module state
+  const moduleIsEnabled = () => {
+    return game.settings.get(
+      "dnd5e-improved-item-rolls",
+      "useImprovedItemRolls"
+    );
+  };
+  Hooks.once("renderChatLog", (app, html, data) => {
+    new ContextMenu(
+      html,
+      ".chat-control-icon",
+      [
+        {
+          name: "Disable Item Rolls",
+          icon: "<i class='fas fa-toggle-off'></i>",
+          condition: moduleIsEnabled,
+          callback: async () => {
+            await game.settings.set(
+              "dnd5e-improved-item-rolls",
+              "useImprovedItemRolls",
+              false
+            );
+          },
+        },
+        {
+          name: "Enable Item Rolls",
+          icon: "<i class='fas fa-toggle-on'></i>",
+          condition: () => !moduleIsEnabled(),
+          callback: async () => {
+            await game.settings.set(
+              "dnd5e-improved-item-rolls",
+              "useImprovedItemRolls",
+              true
+            );
+          },
+        },
+      ],
+      "contextmenu"
+    );
+  });
+
   // Implementing ImprovedItemRolls and Configuring settings
   const useImprovedItemRolls = UseImprovedItemRolls();
   game.settings.register("dnd5e-improved-item-rolls", "useImprovedItemRolls", {
